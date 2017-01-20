eafbe751794abcca761c00adc97178274476283f
tianqi <ltq335061914@yahoo.co.jp>
2017-01-20 01:57:05 +0000

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120014935) do
+ActiveRecord::Schema.define(version: 20170120015238) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 20170120014935) do
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_taggings_on_book_id"
     t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
